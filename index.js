a99e7f265e736f85cff2cb09ba59095c109e7b7f
Sebastien Dumetz <sebastien.dumetz@isen-lille.fr>
2015-06-03 12:17:20 +0200

    methods to search for input streams instead of hardcoding them

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 
 var read_device = require("./lib");
 
-var device_streams = new read_device({devices:["/dev/input/by-path/pci-0000:45:00.0-usb-0:1:1.0-event-joystick"]});
-
+//var device_streams = new read_device({devices:["/dev/input/by-path/pci-0000:45:00.0-usb-0:1:1.0-event-joystick"]});
+var device_streams = new read_device({});
 device_streams.on("EV_KEY",function(data){
   console.log("key : ",data.code,data.value);
 })
+streams = device_streams.search("/dev/input/by-path","event-joystick",function(err,streams){
+  if(err){
+    console.log("node-evdev search stream : ", err);
+  }
+});
